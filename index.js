34f6d934128189db9f3f4027bf1981695f4b5c1c
Bhargav-Tibadiya <bhargavptibadiya.tbp@gmail.com>
2023-11-07 21:50:13 +0530

    Added HTML

--- index.js
+++ index.js
@@ -1,40 +1,80 @@
-const api_key = 'cb690d983d68ef169ff9bbc5201fb01d';
 
 
-console.log('Hello Jee');
-
-async function showWeather() {
-
-    try {
-        let latitude = 15.3333;
-        let longitudde = 74.0833;
-
-        let city = "Goa";
 
 
-        const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`);
-        console.log("Responce get");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const api_key = 'cb690d983d68ef169ff9bbc5201fb01d';
+
+
+// console.log('Hello Jee');
+
+// async function showWeather() {
+
+//     try {
+//         let latitude = 15.3333;
+//         let longitudde = 74.0833;
+
+//         let city = "Goa";
+
+
+//         const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`);
+//         console.log("Responce get");
     
-        const data = await responce.json();
-        console.log("Converted to JSON");
+//         const data = await responce.json();
+//         console.log("Converted to JSON");
     
-        console.log("Weather Data : ", data);
+//         console.log("Weather Data : ", data);
     
-        let newPara = document.createElement('p');
-        newPara.textContent= ` ${data?.main?.temp.toFixed(2)} °C`;
-        document.body.appendChild(newPara);
+//         let newPara = document.createElement('p');
+//         newPara.textContent= ` ${data?.main?.temp.toFixed(2)} °C`;
+//         document.body.appendChild(newPara);
         
-    } catch (error) {
-        console.log(error);   
-    }
-}
+//     } catch (error) {
+//         console.log(error);   
+//     }
+// }
 
-async function getCustomWeather(){
-    let latitude = 15.6333;
-    let longitude = 18.3333;
+// async function getCustomWeather(){
+//     let latitude = 15.6333;
+//     let longitude = 18.3333;
 
-    let responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`);
-    let data = await responce.json(); 
+//     let responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`);
+//     let data = await responce.json(); 
 
-    console.log(data);   
-}
+//     console.log(data);   
+// }
