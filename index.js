54cc33a7448606e44fe3adc3994708360989b285
Bhargav-Tibadiya <bhargavptibadiya.tbp@gmail.com>
2023-11-08 17:05:02 +0530

    UPDATED JS FILE

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const userTab =  document.querySelector("[data-userWeather]");
+const serachTab =  document.querySelector("[data-searchWeather]");
+const userContainer =  document.querySelector(".weather-container");
 
+const grantAccessController= document.querySelector("grant-location-container");
+const searchForm = document.querySelector(".data-searchForm");
 
 
 
